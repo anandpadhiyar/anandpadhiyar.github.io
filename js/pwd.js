6990826a0fcb947c11488fbbe8f1c4e9264ce36f
anandpadhiyar <47624106+anandpadhiyar@users.noreply.github.com>
2019-08-30 09:22:27 +0530

    Update pwd.js

--- js/pwd.js
+++ js/pwd.js
@@ -2,7 +2,7 @@ function checkPwd(){
 	var cpwd = "Rsk22@%&";
 	var pwd = document.getElementById("pwd").value;
 	if(cpwd == pwd)
-		window.location.href="html/homePage.html";
+		window.location.href="html/HomePage.html";
 	else
 		alert("Password is not correct");
 	return false;
